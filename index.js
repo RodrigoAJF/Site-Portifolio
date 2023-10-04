86b3db4be30343fabd761d6a5400b319eb4eb711
RodrigoAJF <rodrigoalvesjacomefilho@gmail.com>
2023-10-04 19:45:17 -0300

    Firts commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function onClickScollTo(id) {
+  document.getElementById(id).scrollIntoView();
+}
+
+function onclickExperiencias() {
+  alert("Sem experiencias profissionais ainda!");
+}
+
+function onClickLink(url) {
+  window.open(url);
+}
